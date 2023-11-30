8b39b098b6c42a53047b7d2da2ca5f8e692f5be6
zuilee <juilee.katpatal@gmail.com>
2023-11-29 18:00:27 -0800

    added socket client

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,22 @@ import { useState, useEffect} from 'react'
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 import { useSelector } from 'react-redux';
+import io from 'socket.io-client';
 
 const Home = () => {
     const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
     const userId = useSelector((state) => state.user.userId)
     
     useEffect(() => {
+
+      // create a socket on 3001 port
+      const socket = io('http://localhost:3001');
+      // SOCKET: fetch blogs using user id 
+      socket.emit('getMyHome', userId);
+      socket.on('blogs', (blogs) => {
+        console.log(`Received `, blogs);
+      });
+
         // test our backend fetch
         const fetchData = async () => {
             try {
